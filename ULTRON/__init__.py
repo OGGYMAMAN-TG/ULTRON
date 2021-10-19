ae110ac68a25e1bab851161cfd8182d9407784a4
OGGYKUTTAN1 <85207400+irash1234567@users.noreply.github.com>
2021-10-19 14:58:54 +0530

    Update __init__.py

--- ULTRON/__init__.py
+++ ULTRON/__init__.py
@@ -1,1 +1,16 @@
+# pylint: disable=missing-module-docstring
+#
+# Copyright (C) 2020-2021 by ultroidTeam@Github, < https://github.com/irash1234567 >.
+#
+# This file is part of < https://github.com/irash1234567/ULTRON > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/irash1234567/ULTRON/blob/master/LICENSE >
+#
+# All rights reserved.
 
+from userge.logger import logging  # noqa
+from userge.config import Config, get_version  # noqa
+from userge.core import (  # noqa
+    Userge, filters, Message, get_collection, pool)
+
+ULTRON = ULTRON()  # ultron is the client name
