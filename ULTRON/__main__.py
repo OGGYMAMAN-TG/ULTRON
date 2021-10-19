fa739f4c19a311e265f2c5bc1c03fe7216da2dd9
OGGYKUTTAN1 <85207400+irash1234567@users.noreply.github.com>
2021-10-19 15:01:02 +0530

    Create __main__.py

---
+++ ULTRON/__main__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-module-docstring
+#
+# Copyright (C) 2020-2021 by UsergeTeam@Github, < https://github.com/irash1234567 >.
+#
+# This file is part of < https://github.com/irash1234567/ULTRON > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/irash1234567/ULTRON/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from userge import userge
+
+if __name__ == "__main__":
+    ultron.begin()
